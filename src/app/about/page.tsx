062399bdefdf0a7bd1c1a93098fc046750f6b659
tom_szpak <tszpak@brandwatch.com>
2025-11-08 18:55:13 +0000

    Adds another placeholder paragraph
    Adds a second paragraph of placeholder text to the about page to increase content and visual length.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,6 +5,7 @@ export default () => {
     <div className="mt-20">
       <h1 className="text-6xl font-bold">Tom Szpak</h1>
       <p className="mt-6 text-xl">Lorem ipsum</p>
+      <p className="mt-6 text-xl">Lorem ipsum</p>
     </div>
   );
 };
